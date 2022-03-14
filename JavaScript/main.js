3f636aa562c44202123e386bd43bcec1b4b8b68b
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-03-14 16:57:11 -0300

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+  });
+}
+
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
+/*Efeito de click*/
+
+window.onclick = myFunction;
+  function myFunction() {
+document.getElementsByClassName("texto")[0].innerHTML = "Sorte de hoje: você vai votar 13 em outubro! ";
+
+}
+
+
